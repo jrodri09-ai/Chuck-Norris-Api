e1ac2fe91d793c162683dd101ae00ac1c0cfad11
Jessica Rodriquez <jrodriquez15@cnm.edu>
2021-08-06 17:21:01 -0600

    frontend built, started on backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import Chuck from './ChuckNorris.jpeg'
+import axios from 'axios';
 
 function App() {
+
+ useEffect( () => {
+   fetchData();
+ }, []);
+
+ const fetchData = async () => {
+  const result = await axios.get('https://api.chucknorris.io/jokes/random');
+  console.log(result.data.value);
+ }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="container">
+     <div className="row">
+       <div className="col-6">
+         <h1 className="title">Chuck Norris API</h1>
+         <img src={Chuck} alt="Chuck Norris"/>
+       </div>
+
+       <div className="col-6 searchJokeCol">
+         <div className="card">
+           <div className="card-header">
+             Search for a word
+           </div>
+           <div className="card-body">
+             <input type="text"/>
+           </div>
+         </div>
+
+         <div>
+           <button className="btn btn-warning btn-lg">Generate Joke</button>
+         </div>
+
+       </div>
+
+
+     </div>
+   <h2 className="subTitle">Here is the Joke</h2>
+   <h4>Chuck Norris is not afraid of anything</h4>
+   </div>
   );
 }
 
